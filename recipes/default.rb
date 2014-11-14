b8c9b17f2aa1cdb02b58bba73314e7801c19205c
adinata <mail.dieend@gmail.com>
2014-11-14 13:23:20 +0700

    set default to install and configure

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,7 @@
 # _|     _||     _||     _||     _||     _||     _||     _|
 #(_ P _ ((_ H _ ((_ P _ ((_ - _ ((_ F _ ((_ P _ ((_ M _ (_ 
 #  |_( )__||_( )__||_( )__||_( )__||_( )__||_( )__||_( )__|
+
+include_recipe "php5-fpm::install"
+include_recipe "php5-fpm::configure_fpm"
+include_recipe "php5-fpm::configure_pools"
